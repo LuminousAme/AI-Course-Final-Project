2649701b8a5c595f500eea4554be84cd689f212f
Jackie <jzhou39@gmail.com>
2023-04-06 19:19:57 -0400

    soudns stuff + some coments

--- Assets/Scripts/EndlessRunnerAgent.cs
+++ Assets/Scripts/EndlessRunnerAgent.cs
@@ -9,19 +9,27 @@ public class EndlessRunnerAgent : Agent
 {
     Rigidbody body;
     ObstacleSpawner spawner;
-    [SerializeField] bool isGrounded = false;
-    [SerializeField] LayerMask ground;
-    [SerializeField] float validGroundDistance = 1f;
+
+    // Ground detection variables
+    [SerializeField] bool isGrounded = false;   // Whether the agent is currently on the ground
+    [SerializeField] LayerMask ground;          // The ground layer
+    [SerializeField] float validGroundDistance = 1f;   // The distance from the ground at which the agent is considered grounded
+
     [SerializeField] float jumpForceMultiplier = 20f;
 
-    Vector3 initalPosition;
-    Vector3 closestObstaclePosition;
-    float timeSinceJump = 0.0f;
+    //audio sources 
+    [SerializeField] private AudioSource deathAudioSource;
+    [SerializeField] private AudioSource jumpAudioSource;
+
+    Vector3 initalPosition;  // The initial position of the agent
+    Vector3 closestObstaclePosition;// The position of the closest obstacle to the agent
+    float timeSinceJump = 0.0f;     // The amount of time since the agent last jumped
 
 
     void Start()
     {
         body = GetComponent<Rigidbody>();
+        // Save the agent's initial position and set grounded status and time since jump
         initalPosition = transform.localPosition;
         isGrounded = false;
         timeSinceJump = 1.0f;
@@ -29,8 +37,10 @@ public class EndlessRunnerAgent : Agent
 
     private void Update()
     {
+        // Increment time since jump
         timeSinceJump += Time.deltaTime;
 
+        // Raycast downwards to check if the agent is on the ground
         if (Physics.Raycast(transform.position, Vector3.down, validGroundDistance, ground))
         {
             isGrounded = true;
@@ -38,6 +48,7 @@ public class EndlessRunnerAgent : Agent
         else isGrounded = false;
 
 
+        // Get a list of all obstacles in the scene and find the closest one to the agent
         List<GameObject> obstacles = spawner.GetObstacles();
         if (obstacles.Count < 0) return;
 
@@ -59,11 +70,13 @@ public class EndlessRunnerAgent : Agent
 
     public override void OnEpisodeBegin()
     {
+        // Reset the agent's position to its initial position
         transform.localPosition = initalPosition;
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
+        // Add the agent's grounded status, y position, and position of the closest obstacle to the sensor
         sensor.AddObservation(isGrounded);
         sensor.AddObservation(transform.position.y);
         sensor.AddObservation(closestObstaclePosition);
@@ -71,20 +84,29 @@ public class EndlessRunnerAgent : Agent
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
+        // Set the continuous action to the vertical input axis
         ActionSegment<float> actions = actionsOut.ContinuousActions;
         actions[0] = Input.GetAxisRaw("Vertical");
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
+        // Get the continuous action input for jumping
         float jumpInput = actions.ContinuousActions[0];
 
-        if(jumpInput > 0.5f && jumpInput <= 1f && isGrounded && timeSinceJump > 0.5f)
+        // Check if the jump input is within a valid range and the agent is on the ground and has not jumped recently
+        if (jumpInput > 0.5f && jumpInput <= 1f && isGrounded && timeSinceJump > 0.5f)
         {
+            // Apply force to the rigidbody to make the agent jump
             body.AddForce(Vector3.up * jumpForceMultiplier);
             timeSinceJump = 0.0f;
+            // Add a positive reward for successful jump
             AddReward(0.1f);
+            // Play sound effect
+            jumpAudioSource.Play();
+
         }
+        // If the jump input is not within the valid range, negative reward
         else if (jumpInput > 1f)
         {
             AddReward(-2f);
@@ -95,20 +117,29 @@ public class EndlessRunnerAgent : Agent
     {
         if(other.gameObject.TryGetComponent(out Obstacle _obstacle))
         {
+            // Play sound effect
+            deathAudioSource.Play();
+            // negative rewards for hitting an obstacle
             AddReward(-30f);
+            // extra negative rewards for hitting an obstacle while on the ground
             if (isGrounded) AddReward(-30f);
+            // Restart the obstacle spawner and end the episode
             spawner.Restart();
             EndEpisode();
+
+       
         }
     }
 
     public void Reward()
     {
+        // Add a positive reward
         AddReward(10f);
     }
 
     public void SetSpawner(ObstacleSpawner spawner)
     {
+        // Set the obstacle spawner
         this.spawner = spawner;
     }
 }
